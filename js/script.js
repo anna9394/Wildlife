b4f5aceb2a28b79db0792d3ba067df32d6221cf6
Anna <annaandreevna90394@gmail.com>
2021-02-04 19:45:11 +0300

    feat: add adaptive main

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ $(".owl-carousel").owlCarousel({
   loop: true,
   nav: true,
   dots: false,
+  margin: 40,
+  navText: ["<img src='../img/left.png'>", "<img src='../img/right.png'>"],
   responsive: {
     0: {
-      items: 1,
+      items: 3,
     },
   },
 });
